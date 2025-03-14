f9017214f2b30e5cb235e2717ff92774c95b1bb6
Shup04 <Bradleyschmidt04@gmail.com>
2025-03-14 13:14:37 -0700

    fixed rating nav error

--- Src/app/src/main/java/com/example/mylibraryapp/SecondActivity.java
+++ Src/app/src/main/java/com/example/mylibraryapp/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.mylibraryapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
@@ -15,6 +16,9 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second_activity);
+
+        Button login_button = findViewById(R.id.rate_books_button);
+        login_button.setOnClickListener(this::rateBooks);
     }
 
     // Search function
